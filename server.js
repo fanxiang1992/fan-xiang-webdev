932b43a235127f815349ecea9fd6aa4f9bdb4cf1
Xiang Fan <xiangfan1992@gmail.com>
2016-11-06 23:58:29 -0500

    link github

--- server.js
+++ server.js
@@ -27,7 +27,17 @@ app.use(express.static(__dirname + '/public'));
 require ("./test/app.js")(app);
 require ("./assignment/app.js")(app);
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
 
-app.listen(port, ipaddress);
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+//var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+//var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+//app.listen(port, ipaddress);
